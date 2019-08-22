b48d935552f12d6846b30fa9b418056fe2a9607c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-21 23:58:46 -0400

    Adds automated error handling

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
-
-const authRouter = require('./auth/auth-router.js');
-const usersRouter = require('./users/usersRouter.js');
-
-const publicJokesRouter = require('./publicJokes/publicJokesRouter.js');
-const privateJokesRouter = require('./PrivateJokes/privateJokesRouter.js');
-
 const server = express();
+
+// Routes
+// const authRouter = require('./routes/auth/');
+const usersRouter = require('./routes/users/');
+const jokesRouter = require('./routes/jokes/');
+const favoritesRouter = require('./routes/favorites/');
+const upvotesRouter = require('./routes/upvotes/');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-server.use('/api/auth', authRouter);
+// server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
-server.use('/api/publicJokes', publicJokesRouter);
-server.use('/api/privateJokes', privateJokesRouter);
+server.use('/api/jokes', jokesRouter);
+server.use('/api/favorites', favoritesRouter);
+server.use('/api/upvotes', upvotesRouter);
 
 server.get('/', (req, res) => {
   res.send("It's working!");
 });
+
+//async error handling middleware MUST come after routes or else will just throw Type error
+server.use(errorHandler);
 
 module.exports = server;
