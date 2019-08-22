84081a4c68199efc445bb1158f4de7cef563a90f
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-22 14:32:28 -0400

    Fixes spelling error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 require('express-async-errors');
-const server = require('./server.js/index.js');
+const server = require('./server.js');
 
 const port = process.env.PORT || 5000;
-server.listen(port, () => console.log(`\n** Runnnig on port ${port} ** \n`));
+server.listen(port, () => console.log(`\n** Running on port ${port} ** \n`));
