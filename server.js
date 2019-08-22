9cd1580343a946ff20377eabf55b5431bf3e2c6d
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-22 14:30:17 -0400

    Commented out routes that are created yet

--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const helmet = require('helmet');
 const cors = require('cors');
 const server = express();
 
+const errorHandler = require('./middleware/errorHandling.js');
 // Routes
-// const authRouter = require('./routes/auth/');
-const usersRouter = require('./routes/users/');
-const jokesRouter = require('./routes/jokes/');
-const favoritesRouter = require('./routes/favorites/');
-const upvotesRouter = require('./routes/upvotes/');
+const authRouter = require('./routes/auth/');
+// const usersRouter = require('./routes/users/');
+// const jokesRouter = require('./routes/jokes/');
+// const favoritesRouter = require('./routes/favorites/');
+// const upvotesRouter = require('./routes/upvotes/');
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-// server.use('/api/auth', authRouter);
-server.use('/api/users', usersRouter);
-server.use('/api/jokes', jokesRouter);
-server.use('/api/favorites', favoritesRouter);
-server.use('/api/upvotes', upvotesRouter);
+server.use('/api/auth', authRouter);
+// server.use('/api/users', usersRouter);
+// server.use('/api/jokes', jokesRouter);
+// server.use('/api/favorites', favoritesRouter);
+// server.use('/api/upvotes', upvotesRouter);
 
 server.get('/', (req, res) => {
   res.send("It's working!");
