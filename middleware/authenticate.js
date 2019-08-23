75810bfd15ff1293d7aaecea23de604943906591
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-22 21:31:48 -0400

    Fixes issues with tokens authorizating correctly

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const secret = require('../config/secret.js');
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization || req.headers.Authorization;
+  const token = req.headers.authorization;
   jwt.verify(token, secret, (err, decodedToken) => {
     if (err) {
       res.status(401).json({ message: 'Invalid token' });
     } else {
-      res.locals.decodedToken = decodedToken;
+      res.locals.token = decodedToken;
       next();
     }
   });
