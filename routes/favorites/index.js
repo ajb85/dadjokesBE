0f36a94d85054f7459370bd975b185ea15ace490
AJ Brush <19802883+ajb85@users.noreply.github.com>
2019-08-29 00:38:38 -0400

    Adds isPublic check for favoriting

--- routes/favorites/index.js
+++ routes/favorites/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Favorites = require('../../models/db/favorites.js');
+const Jokes = require('../../models/db/jokes.js');
 
 router.route('/').get(async (req, res) => {
   const { user_id } = res.locals.token;
@@ -15,13 +16,18 @@ router.route('/').get(async (req, res) => {
 router.route('/toggle/:joke_id').post(async (req, res) => {
   const { user_id } = res.locals.token;
   const { joke_id } = req.params;
-  const toggled = await Favorites.toggle({ user_id, joke_id });
 
-  return toggled.id
-    ? res.status(200).json(toggled)
-    : toggled
-    ? res.sendStatus(200)
-    : res.status(404).json({ message: "I didn't find a joke to toggle" });
+  const joke = await Jokes.find({ 'j.id': joke_id }).first();
+
+  if (joke && (joke.user_id === user_id || joke.isPublic)) {
+    await Favorites.toggle({ user_id, joke_id });
+    const users_favorites = await Favorites.find({ 'f.user_id': user_id });
+
+    return res.status(200).json(users_favorites);
+  } else
+    return res.status(404).json({
+      message: "That joke doesn't exist or you don't own the private joke."
+    });
 });
 
 module.exports = router;
